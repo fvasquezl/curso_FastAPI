6156ed996174f63357e8715c353deac1bbab6ea1
Faustino Vasquez <vasquez.limon@gmail.com>
2024-03-28 16:12:54 -0700

    status code

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ $ uvicorn main:app --reload --port 5000 --host 0.0.0.0
 ```
 localhost:5000/docs
 ```
+## Information
+### HTTP response status code 
+```
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+
+-Informational responses (100 – 199)
+-Successful responses    (200 – 299)
+-Redirection messages    (300 – 399)
+-Client error responses  (400 – 499)
+-Server error responses  (500 – 599)
+
+```
--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Path, Query
+from fastapi.responses import HTMLResponse, JSONResponse
+from pydantic import BaseModel, Field
+from typing import Optional, List
 
 app = FastAPI()
 app.title = "My App with FasAPI"
 app.version = "0.0.1"
 
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=15, max_length=50)
+    year: int = Field(le=2024)
+    rating: float = Field(ge=1, le=10)
+    category: str = Field(min_length=5, max_length=15)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "title": "Mi pelicula",
+                "overview": "Descriprion de la pelicula",
+                "year": 2024,
+                "rating": 9.8,
+                "category": "Accion",
+            }
+        }
+
+
 movies = [
-      {
+    {
         "id": 1,
         "title": "Avatar",
         "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
         "year": "2009",
         "rating": 7.8,
-        "category": "Acción"
+        "category": "Acción",
     },
     {
         "id": 2,
@@ -20,65 +44,75 @@ movies = [
         "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
         "year": "2009",
         "rating": 7.8,
-        "category": "Acción"
+        "category": "Acción",
     },
-        {
+    {
         "id": 3,
         "title": "Avatar",
         "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
         "year": "2009",
         "rating": 7.8,
-        "category": "Acción"
-    }
+        "category": "Acción",
+    },
 ]
 
-@app.get('/',tags=['home'])
+
+@app.get("/", tags=["home"])
 def message():
-    return HTMLResponse('<h1>Hello World</h1>')
+    return HTMLResponse("<h1>Hello World</h1>")
 
-@app.get('/movies',tags=['movies'])
-def get_movies():
-    return movies
 
-@app.get('/movies/{id}',tags=['movies'])
-def get_movies(id: int):
+@app.get("/movies", tags=["movies"], response_model=List[Movie], status_code=200)
+def get_movies() -> List[Movie]:
+    return JSONResponse(status_code=200, content=movies)
+
+
+@app.get("/movies/{id}", tags=["movies"], response_model=Movie, status_code=200)
+def get_movies(id: int = Path(ge=1, le=2000)) -> Movie:
     for item in movies:
         if item["id"] == id:
-          return item
-    return []
+            return JSONResponse(status_code=200, content=item)
+    return JSONResponse(status_code=200, content=[])
 
-@app.get('/movies/',tags=['movies'])
-def get_movies_by_category(category:str,year:int):
-    items = [item for item in movies if item['category']==category]
-    return items
 
-@app.post('/movies/',tags=['movies'])
-def create_movies(id:int=Body(), title:str=Body(), overview:str=Body(),year:int=Body(),rating:float=Body(),category:str=Body()):
-    movies.append({
-        "id": id,
-        "title": title,
-        "overview": overview,
-        "year": str(year),
-        "rating": rating,
-        "category": category
-    })
-    return movies
+@app.get("/movies/", tags=["movies"], response_model=List[Movie], status_code=200)
+def get_movies_by_category(
+    category: str = Query(min_length=5, max_length=15)
+) -> List[Movie]:
+    data = [item for item in movies if item["category"] == category]
+    return JSONResponse(status_code=200, content=data)
 
-@app.put('/movies/{id}',tags=['movies'])
-def update_movies(id:int, title:str=Body(), overview:str=Body(),year:int=Body(),rating:float=Body(),category:str=Body()):
-    for movie in movies:
-        if movie['id'] == id:
-            movie['title']= title
-            movie["overview"]= overview
-            movie["year"]= year
-            movie["rating"] =rating
-            movie["category"]= category
-    return movies
 
-@app.delete('/movies/{id}',tags=['movies'])
-def delete_movies(id:int):
-    for movie in movies:
-        if movie['id'] == id:
-           index = movies.index(movie)
-           movies[index]=''
-    return movies
+@app.post("/movies/", tags=["movies"], response_model=dict, status_code=201)
+def create_movies(movie: Movie) -> dict:
+    movies.append(movie)
+    return JSONResponse(
+        status_code=201, content={"message": "Se ha registrado la pelicula"}
+    )
+
+
+@app.put("/movies/{id}", tags=["movies"], response_model=dict, status_code=200)
+def update_movies(id: int, movie: Movie) -> dict:
+    for item in movies:
+        if item["id"] == id:
+            item["title"] = movie.title
+            item["overview"] = movie.overview
+            item["year"] = movie.year
+            item["rating"] = movie.rating
+            item["category"] = movie.category
+        return JSONResponse(
+            status_code=200, content={"message": "Se ha modificado la pelicula"}
+        )
+
+
+@app.delete("/movies/{id}", tags=["movies"], response_model=dict, status_code=200)
+def delete_movies(id: int) -> dict:
+    for item in movies:
+        if item["id"] == id:
+            movies.remove(item)
+        return JSONResponse(
+            status_code=200, content={"message": "Se ha eliminado la pelicula"}
+        )
+    return JSONResponse(
+        status_code=404, content={"message": "No se encontro ningun registro"}
+    )
