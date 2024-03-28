570ae0aaa438012b46aa8f56d10d9399c49cc262
Faustino Vasquez <vasquez.limon@gmail.com>
2024-03-27 23:36:43 -0700

    crud complete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -16,6 +16,14 @@ movies = [
     },
     {
         "id": 2,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    },
+        {
+        "id": 3,
         "title": "Avatar",
         "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
         "year": "2009",
@@ -43,3 +51,34 @@ def get_movies(id: int):
 def get_movies_by_category(category:str,year:int):
     items = [item for item in movies if item['category']==category]
     return items
+
+@app.post('/movies/',tags=['movies'])
+def create_movies(id:int=Body(), title:str=Body(), overview:str=Body(),year:int=Body(),rating:float=Body(),category:str=Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": str(year),
+        "rating": rating,
+        "category": category
+    })
+    return movies
+
+@app.put('/movies/{id}',tags=['movies'])
+def update_movies(id:int, title:str=Body(), overview:str=Body(),year:int=Body(),rating:float=Body(),category:str=Body()):
+    for movie in movies:
+        if movie['id'] == id:
+            movie['title']= title
+            movie["overview"]= overview
+            movie["year"]= year
+            movie["rating"] =rating
+            movie["category"]= category
+    return movies
+
+@app.delete('/movies/{id}',tags=['movies'])
+def delete_movies(id:int):
+    for movie in movies:
+        if movie['id'] == id:
+           index = movies.index(movie)
+           movies[index]=''
+    return movies
