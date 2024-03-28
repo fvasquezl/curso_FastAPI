0d13caed7d7ecb4da10b216f1ec473ea86aa47cb
Faustino Vasquez <vasquez.limon@gmail.com>
2024-03-27 22:30:28 -0700

    parameters an querystring

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ movies = [
         "year": "2009",
         "rating": 7.8,
         "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
     }
 ]
 
@@ -23,3 +31,15 @@ def message():
 @app.get('/movies',tags=['movies'])
 def get_movies():
     return movies
+
+@app.get('/movies/{id}',tags=['movies'])
+def get_movies(id: int):
+    for item in movies:
+        if item["id"] == id:
+          return item
+    return []
+
+@app.get('/movies/',tags=['movies'])
+def get_movies_by_category(category:str,year:int):
+    items = [item for item in movies if item['category']==category]
+    return items
