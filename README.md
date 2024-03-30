25fa9df90da58e6872613c015be81f82d34bfbed
Faustino Vasquez <vasquez.limon@gmail.com>
2024-03-29 23:28:14 -0700

    refactorization

--- README.md
+++ README.md
@@ -12,16 +12,14 @@ $ uvicorn main:app --reload --port 5000 --host 0.0.0.0
 ```
 localhost:5000/docs
 ```
-## Information
-### HTTP response status code 
+## Libraries
 ```
-https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-
-
--Informational responses (100 – 199)
--Successful responses    (200 – 299)
--Redirection messages    (300 – 399)
--Client error responses  (400 – 499)
--Server error responses  (500 – 599)
+Api: fastapi
+Tokens: PyJWT
+Orm: SQLAlchemy
+WebServer: uvicorn
 
 ```
+
+
+
--- main.py
+++ main.py
@@ -1,82 +1,20 @@
-from fastapi import Depends, FastAPI, HTTPException, Path, Query, Request
-from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.security.http import HTTPAuthorizationCredentials
-from pydantic import BaseModel, Field
-from typing import Coroutine, Optional, List
-from jwt_manager import create_token, validate_token
-from fastapi.security import HTTPBearer
-from config.database import Session, engine, Base
-from models.movie import Movie as MovieModel
-from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from config.database import engine, Base
+from middlewares.error_handler import ErrorHandler
+from routers.movie import movie_router
+from routers.users import user_router
 
 
 app = FastAPI()
 app.title = "My App with FasAPI"
 app.version = "0.0.1"
+app.add_middleware(ErrorHandler)
+app.include_router(movie_router)
+app.include_router(user_router)
 
 Base.metadata.create_all(bind=engine)
 
-
-class JWTBearer(HTTPBearer):
-    async def __call__(self, request: Request):
-        auth = await super().__call__(request)
-        data = validate_token(auth.credentials)
-        if (data["email"]) != "admin@gmail.com":
-            raise HTTPException(status_code=403, details="Credenciales son invalidas")
-
-
-class User(BaseModel):
-    email: str
-    password: str
-
-
-class Movie(BaseModel):
-    id: Optional[int] = None
-    title: str = Field(min_length=5, max_length=15)
-    overview: str = Field(min_length=15, max_length=50)
-    year: int = Field(le=2024)
-    rating: float = Field(ge=1, le=10)
-    category: str = Field(min_length=5, max_length=15)
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "id": 1,
-                "title": "Mi pelicula",
-                "overview": "Descriprion de la pelicula",
-                "year": 2024,
-                "rating": 9.8,
-                "category": "Accion",
-            }
-        }
-
-
-movies = [
-    {
-        "id": 1,
-        "title": "Avatar",
-        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
-        "year": "2009",
-        "rating": 7.8,
-        "category": "Acción",
-    },
-    {
-        "id": 2,
-        "title": "Avatar",
-        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
-        "year": "2009",
-        "rating": 7.8,
-        "category": "Acción",
-    },
-    {
-        "id": 3,
-        "title": "Avatar",
-        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
-        "year": "2009",
-        "rating": 7.8,
-        "category": "Acción",
-    },
-]
 
 
 @app.get("/", tags=["home"])
@@ -84,75 +22,4 @@ def message():
     return HTMLResponse("<h1>Hello World</h1>")
 
 
-@app.post("/login", tags=["auth"])
-def login(user: User):
-    if user.email == "admin@gmail.com" and user.password == "admin":
-        token: str = create_token(user.model_dump())
-        return JSONResponse(status_code=200, content=token)
 
-
-@app.get(
-    "/movies",
-    tags=["movies"],
-    response_model=List[Movie],
-    status_code=200,
-    dependencies=[Depends(JWTBearer())],
-)
-def get_movies() -> List[Movie]:
-    db = Session()
-    result = db.query(MovieModel).all()
-
-    return JSONResponse(status_code=200, content=jsonable_encoder(result))
-
-
-@app.get("/movies/{id}", tags=["movies"], response_model=Movie, status_code=200)
-def get_movies(id: int = Path(ge=1, le=2000)) -> Movie:
-    db = Session()
-    result = db.query(MovieModel).filter(MovieModel.id == id).first()
-    if not result:
-        return JSONResponse(status_code=404, content={"message": "No encontrado"})
-
-    return JSONResponse(status_code=200, content=jsonable_encoder(result))
-
-
-@app.get("/movies/", tags=["movies"], response_model=List[Movie], status_code=200)
-def get_movies_by_category(
-    category: str = Query(min_length=5, max_length=15)
-) -> List[Movie]:
-    data = [item for item in movies if item["category"] == category]
-    return JSONResponse(status_code=200, content=data)
-
-
-@app.post("/movies/", tags=["movies"], response_model=dict, status_code=201)
-def create_movies(movie: Movie) -> dict:
-    db = Session()
-    new_movie = MovieModel(**movie.model_dump())
-    db.add(new_movie)
-    db.commit()
-    return JSONResponse(
-        status_code=201, content={"message": "Se ha registrado la pelicula"}
-    )
-
-
-@app.put("/movies/{id}", tags=["movies"], response_model=dict, status_code=200)
-def update_movies(id: int, movie: Movie) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            item["title"] = movie.title
-            item["overview"] = movie.overview
-            item["year"] = movie.year
-            item["rating"] = movie.rating
-            item["category"] = movie.category
-        return JSONResponse(
-            status_code=200, content={"message": "Se ha modificado la pelicula"}
-        )
-
-
-@app.delete("/movies/{id}", tags=["movies"], response_model=dict, status_code=200)
-def delete_movies(id: int) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            movies.remove(item)
-        return JSONResponse(
-            status_code=200, content={"message": "Se ha eliminado la pelicula"}
-        )
